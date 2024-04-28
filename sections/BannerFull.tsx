4a1a90fad21ff80e6af1665859980093600a77cf
vmorais34 <vmorais34@gmail.com>
2024-04-28 17:06:38 -0300

    fix: banner full and links menu

--- sections/BannerFull.tsx
+++ sections/BannerFull.tsx
@@ -1,4 +1,5 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
+import { Picture, Source } from "apps/website/components/Picture.tsx";
 import Image from "apps/website/components/Image.tsx";
 
 interface Props {
@@ -8,41 +9,37 @@ interface Props {
   photo?: ImageWidget;
 }
 
-export interface Photo {
-  src?: ImageWidget;
-  /** @description text alternative */
-  altText?: string;
-}
-
-
-const IMG_PLACEHODLER ={
-  src:
-    "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/03fbcc78-ca86-4616-a59a-b8aa18331a9c",
-  altText: "Image Placeholder",
-};
-
 export default function BannerFull({ 
-  photo = IMG_PLACEHODLER
+  photo = {
+    src: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8239/b2bc4835-838c-4fcb-acec-a5d766cc3dff",
+    srcMobile: "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/8239/1d6d12e0-3590-40a3-8162-f2de00c24753",
+  },
 }: Props) {
-  console.log("photo", photo);
-  const bannerFull = (
-    <>
-      {photo?.map((item: Photo) => {
-        return (
-          <Image
-            src={`${item.src}`}
-            alt={`${item.altText}`}
-            height={500}
-            width={500}
-            class="rounded"
-          />
-        );
-      })}
-    </>
-  );
+
   return (
     <div class="w-full h-auto lg:mx-auto">
-      {bannerFull}
+      <Picture>
+        <Source
+          media="(max-width: 768px)"
+          src="https://example.com/image-mobile.png"
+          class="w-full h-full"
+          width={270}
+          height={377}
+        />
+        <Source
+          media="(min-width: 768px)"
+          src={photo.src}
+          class="w-full h-full"
+          width={800}
+          height={1200}
+        />
+        <Image
+          src="https://example.com/image.png"
+          class="w-full h-full object-cover"
+          width={800}
+          height={1200}
+        />
+      </Picture>
     </div>
   );
 }
